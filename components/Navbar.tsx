66967057263265922571a578eae75a138dfa8387
rick-sys <ricksanchezy2x@gmail.com>
2024-02-18 23:20:40 +0100

    v6

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,4 +1,5 @@
 import { AvatarIcon } from "@radix-ui/react-icons";
+import { AvatarDemo } from "./ui/avatardemo";
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 import {
@@ -71,6 +72,7 @@ export default async function Navbar() {
             )}
             <DropdownMenu>
               <DropdownMenuTrigger asChild className="cursor-pointer">
+                <AvatarDemo/>
                 <AvatarIcon height={24} width={24} className="text-primary" />
               </DropdownMenuTrigger>
               <DropdownMenuContent className="w-56">
